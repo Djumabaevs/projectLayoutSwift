68b1e7e9891de867059f2f4e8adbd5b813f09dc4
BakytDzhumabev <djumabaevb@gmail.com>
2021-05-20 09:36:57 +0600

    use of view.frame -> width and height

--- projectLayoutSwift/ViewController.swift
+++ projectLayoutSwift/ViewController.swift
@@ -12,6 +12,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let width = view.frame.size.width
+        let height = view.frame.size.height
+        
+        let myLabel = UILabel()
+        myLabel.text = "Super car"
+        myLabel.textAlignment = .center
+        myLabel.frame = CGRect(x: width * 0.5 - width * 0.8 / 2, y: height * 0.5 - 50 / 2, width: width * 0.8, height: 50)
+        view.addSubview(myLabel)
+        
     }
 
 
